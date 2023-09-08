8c806e512c0e73b2e1717efda16765921d9740de
nkverechsh <vereshagin-n@mail.ru>
2023-09-08 05:23:11 +0300

    setup linter and prettier

--- src/lib/templateEngine.js
+++ src/lib/templateEngine.js
@@ -1,43 +1,43 @@
 export function templateEngine(block) {
-    if (block === undefined || block === null || block === false) {
-        return document.createTextNode('');
-    }
-    if (
-        typeof block === 'string' ||
-        typeof block === 'number' ||
-        block === true
-    ) {
-        return document.createTextNode(block);
-    }
-    if (Array.isArray(block)) {
-        const fragment = document.createDocumentFragment();
+  if (block === undefined || block === null || block === false) {
+    return document.createTextNode("");
+  }
+  if (
+    typeof block === "string" ||
+    typeof block === "number" ||
+    block === true
+  ) {
+    return document.createTextNode(block);
+  }
+  if (Array.isArray(block)) {
+    const fragment = document.createDocumentFragment();
 
-        block.forEach((item) => {
-            const el = templateEngine(item);
+    block.forEach((item) => {
+      const el = templateEngine(item);
 
-            fragment.appendChild(el);
-        });
+      fragment.appendChild(el);
+    });
 
-        return fragment;
-    }
+    return fragment;
+  }
 
-    const element = document.createElement(block.tag);
+  const element = document.createElement(block.tag);
 
-    if (block.cls) {
-        element.classList.add(...[].concat(block.cls).filter(Boolean));
-    }
+  if (block.cls) {
+    element.classList.add(...[].concat(block.cls).filter(Boolean));
+  }
 
-    if (block.attrs) {
-        const keys = Object.keys(block.attrs);
+  if (block.attrs) {
+    const keys = Object.keys(block.attrs);
 
-        keys.forEach((key) => {
-            element.setAttribute(key, block.attrs[key]);
-        });
-    }
+    keys.forEach((key) => {
+      element.setAttribute(key, block.attrs[key]);
+    });
+  }
 
-    const content = templateEngine(block.content);
+  const content = templateEngine(block.content);
 
-    element.appendChild(content);
+  element.appendChild(content);
 
-    return element;
+  return element;
 }
--- src/timer.js
+++ src/timer.js
@@ -1,44 +1,44 @@
 export class Timer {
-    constructor() {
-        console.log('new Timer');
-        console.log(window.appState.gameDuration);
-    }
+  constructor() {
+    console.log("new Timer");
+    console.log(window.appState.gameDuration);
+  }
 
-    run() {
-        const startTime = Date.now();
-        this.interval = setInterval(() => {
-            const currentTime = Date.now();
-            window.appState.gameDuration = currentTime - startTime;
-            this.displayTime();
-        }, 1000);
-    }
+  run() {
+    const startTime = Date.now();
+    this.interval = setInterval(() => {
+      const currentTime = Date.now();
+      window.appState.gameDuration = currentTime - startTime;
+      this.displayTime();
+    }, 1000);
+  }
 
-    clear() {
-        clearInterval(this.interval);
-    }
+  clear() {
+    clearInterval(this.interval);
+  }
 
-    displayTime() {
-        if (!this.minutesElement || !this.secondsElement) {
-            return;
-        }
-        this.minutesElement.textContent = this.formatValues(this.getMinutes());
-        this.secondsElement.textContent = this.formatValues(this.getSeconds());
+  displayTime() {
+    if (!this.minutesElement || !this.secondsElement) {
+      return;
     }
+    this.minutesElement.textContent = this.formatValues(this.getMinutes());
+    this.secondsElement.textContent = this.formatValues(this.getSeconds());
+  }
 
-    getMinutes() {
-        return Math.floor(window.appState.gameDuration / 60000);
-    }
+  getMinutes() {
+    return Math.floor(window.appState.gameDuration / 60000);
+  }
 
-    getSeconds() {
-        return Math.floor(window.appState.gameDuration / 1000) % 60;
-    }
+  getSeconds() {
+    return Math.floor(window.appState.gameDuration / 1000) % 60;
+  }
 
-    formatValues(value) {
-        return String(value).padStart(2, 0);
-    }
+  formatValues(value) {
+    return String(value).padStart(2, 0);
+  }
 
-    setTimeElements(minutesEment, secondsElement) {
-        this.minutesElement = minutesEment;
-        this.secondsElement = secondsElement;
-    }
+  setTimeElements(minutesEment, secondsElement) {
+    this.minutesElement = minutesEment;
+    this.secondsElement = secondsElement;
+  }
 }
