8f0051151058546375a9d1dc1f61900613677d00
nkverechsh <vereshagin-n@mail.ru>
2023-09-12 02:49:58 +0300

    hw - Основы Typescript

---
+++ src/main.ts
@@ -0,0 +1,106 @@
+import './style.css';
+import { GameTimer } from './timer';
+import { renderFirstScreen } from './firstScreen';
+import { mixArrays, Cart } from './lib/utilityFunctions';
+const appElement: HTMLElement | null = document.querySelector('.app');
+type Difficulty = {
+    buttonText: string;
+    cartsCount: number;
+    layout: {
+        rows: number;
+        columns: number;
+    };
+};
+declare global {
+    interface Window {
+        appState: {
+            difficulty: string;
+            gameDuration: number;
+            status: string;
+        };
+        timer: GameTimer;
+        DIFFICULTIES: {
+            low: Difficulty;
+            medium: Difficulty;
+            high: Difficulty;
+        };
+        CARTS: Cart[];
+        STATUSES: {
+            ok: string;
+            win: string;
+            lose: string;
+        };
+        END_MESSAGE: {
+            win: string;
+            lose: string;
+        };
+        END_IMAGE_PATH: {
+            win: string;
+            lose: string;
+        };
+        OPACITY: number;
+    }
+}
+
+window.appState = {
+    difficulty: '',
+    gameDuration: 0,
+    status: '',
+};
+
+window.timer = new GameTimer();
+window.DIFFICULTIES = {
+    low: {
+        buttonText: '1',
+        cartsCount: 6,
+        layout: {
+            rows: 2,
+            columns: 3,
+        },
+    },
+    medium: {
+        buttonText: '2',
+        cartsCount: 12,
+        layout: {
+            rows: 3,
+            columns: 4,
+        },
+    },
+    high: {
+        buttonText: '3',
+        cartsCount: 18,
+        layout: {
+            rows: 3,
+            columns: 6,
+        },
+    },
+};
+const SUITS = ['hearts', 'diamonds', 'crosses', 'spades'];
+const RANKS = ['6', '7', '8', '9', '10', 'Q', 'K', 'J', 'A'];
+
+window.CARTS = mixArrays(SUITS, RANKS, 'suit', 'rank');
+
+window.STATUSES = {
+    ok: 'ok',
+    win: 'win',
+    lose: 'lose',
+};
+
+window.END_MESSAGE = {
+    win: 'Вы выиграли!',
+    lose: 'Вы проиграли',
+};
+
+window.END_IMAGE_PATH = {
+    win: './static/end_win.svg',
+    lose: './static/end_lose.svg',
+};
+
+window.OPACITY = 0.3;
+
+console.log(window.CARTS);
+
+if (appElement !== null) {
+    renderFirstScreen(appElement);
+}
+export { appElement };
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,53 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
-const isProduction = process.env.NODE_ENV === "production";
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: "./src/main.js",
-  mode: isProduction ? "production" : "development",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
-  module: {
-    rules: [{
-        test: /\.scss|css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
+    entry: './src/main',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss|css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool: isProduction ? 'hidden-source-map' : 'source-map',
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [{ from: './src/img', to: 'static' }],
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
     ],
-  },
-  optimization: {
-    minimizer: ["...", new CssMinimizerPlugin()],
-  },
-  devtool: isProduction ? "hidden-source-map" : "source-map",
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new CopyPlugin({
-      patterns: [{ from: "./src/img", to: "static" }],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-  ],
+    mode: isProduction ? 'production' : 'development',
 };
